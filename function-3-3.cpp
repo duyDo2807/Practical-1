2eb8c53b23c99795c97d1371de5ba71ede344be5
duyDo2807 <a1881078@adelaide.edu.au>
2023-07-27 22:36:16 +0930

    Practical 1

--- function-3-3.cpp
+++ function-3-3.cpp
@@ -6,40 +6,26 @@ double weighted_average(int array[], int n) {
   }
 
   double weighted_sum = 0.0;
-  int total_count = 0;
+  int count = 0;
 
   for (int i = 0; i < n; i++) {
     if (array[i] == 0) {
-      continue;  // Skip elements that have been processed to avoid double
-                 // counting
+      continue;
     }
 
     int current_element = array[i];
-    double occurrences = 0;
+    double occur = 0;
 
-    // Count the occurrences of the current element
     for (int j = i; j < n; j++) {
       if (array[j] == current_element) {
-        occurrences++;
-      }
-    }
-
-    // Add the weighted value of the current element to the sum
-    weighted_sum += (current_element * occurrences) / n;
-
-    // Increment the total count with the occurrences to avoid division by zero
-    total_count += occurrences;
-
-    // Set occurrences of the current element to 0 to avoid double counting
-    for (int j = i; j < n; j++) {
-      if (array[j] == current_element) {
+        occur++;
         array[j] = 0;
       }
     }
+    weighted_sum = weighted_sum + (current_element * occur) / n;
+    count = count + occur;
   }
-
-  // Avoid division by zero
-  if (total_count == 0) {
+  if (count == 0) {
     return 0;
   }
 
