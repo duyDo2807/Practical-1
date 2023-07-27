e19b45ed7e0c89b941e3d2393af9c62bdc588aff
duyDo2807 <a1881078@adelaide.edu.au>
2023-07-27 22:34:27 +0930

    Practical 1

---
+++ backup.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+double weighted_average(int array[], int n) {
+  if (n < 1) {
+    return 0;
+  }
+
+  double weighted_sum = 0.0;
+  int count = 0;
+
+  for (int i = 0; i < n; i++) {
+    if (array[i] == 0) {
+      continue;
+    }
+
+    int current_element = array[i];
+    double occur = 0;
+
+    for (int j = i; j < n; j++) {
+      if (array[j] == current_element) {
+        occur++;
+        array[j] = 0;
+      }
+    }
+    weighted_sum = weighted_sum + (current_element * occur) / n;
+    count = count + occur;
+  }
+  if (count == 0) {
+    return 0;
+  }
+
+  return weighted_sum;
+}
