8adaea889323dc5d58348a9eb07c883f4fe50da6
duyDo2807 <a1881078@adelaide.edu.au>
2023-07-27 22:29:43 +0930

    Pratical 1

--- function-3-3.cpp
+++ function-3-3.cpp
@@ -4,7 +4,10 @@ double weighted_average(int array[], int n) {
   if (n < 1) {
     return 0;
   }
+
   double weighted_sum = 0.0;
+  int count = 0;
+
   for (int i = 0; i < n; i++) {
     if (array[i] == 0) {
       continue;
@@ -12,20 +15,18 @@ double weighted_average(int array[], int n) {
 
     int current_element = array[i];
     double occur = 0;
-    int count = 0;
 
     for (int j = i; j < n; j++) {
       if (array[j] == current_element) {
         occur++;
-      }
-    }
-    for (int j = i; j < n; j++) {
-      if (array[j] == current_element) {
         array[j] = 0;
       }
     }
     weighted_sum = weighted_sum + (current_element * occur) / n;
     count = count + occur;
+  }
+  if (count == 0) {
+    return 0;
   }
 
   return weighted_sum;
