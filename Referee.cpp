ca2e8e218b80c3cac22ac7ef58967e998854c36c
duyDo2807 <a1881078@adelaide.edu.au>
2024-03-01 21:05:17 +1030

    Practical 1 Y2S1

--- Referee.cpp
+++ Referee.cpp
@@ -5,7 +5,7 @@ Player* Referee::refGame(Player* player1, Player* player2) {
   char play2 = player2->makeMove();
 
   if (play1 == play2) {
-    return nullptr;  // It's a tie
+    return nullptr;
   } else if ((play1 == 'R' && play2 == 'S') || (play1 == 'S' && play2 == 'P') ||
              (play1 == 'P' && play2 == 'R')) {
     return player1;
